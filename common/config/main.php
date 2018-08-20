9efe36941f2c09c54203eca8b2c554d98b4effd1
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-20 17:06:15 +0530

    "init"

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=ltm',
+            'username' => 'root',
+            'password' => 'q123',
+            'charset' => 'utf8',
+        ],
     ],
 ];
