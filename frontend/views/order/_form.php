8a5a262b6fb217496cb3c016891c5619cf7cb4a8
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-23 17:39:19 +0530

    init

--- frontend/views/order/_form.php
+++ frontend/views/order/_form.php
@@ -22,11 +22,11 @@
         <?=$form->field($model,'amount')->textInput(); ?>
         <?=$form->field($model,'mrp')->textInput(); ?>
         <?=$form->field($model,'tax_rate')->textInput(); ?>
-        <?=$form->field($model,'status')->textInput(); ?>
-        <?=$form->field($model,'created_at')->textInput(); ?>
-        <?=$form->field($model,'updated_at')->textInput(); ?>
-        <?=$form->field($model,'created_by')->textInput(); ?>
-        <?=$form->field($model,'updated_by')->textInput(); ?>
+        <?=$form->field($model,'status')->hiddenInput(['value'=> 10])->label(false); ?>
+        <?=$form->field($model,'created_at')->hiddenInput(['value'=> 10])->label(false); ?>
+        <?=$form->field($model,'updated_at')->hiddenInput(['value'=> 10])->label(false); ?>
+        <?=$form->field($model,'created_by')->hiddenInput(['value'=> 10])->label(false); ?>
+        <?=$form->field($model,'updated_by')->hiddenInput(['value'=> 10])->label(false); ?>
         
         <div class="form-group">
             <?= \yii\helpers\Html::submitButton('Save', [
