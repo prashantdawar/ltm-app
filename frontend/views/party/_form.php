971342ca296802f69c442c011d48a9af5ce24888
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-24 16:04:21 +0530

    init

--- frontend/views/party/_form.php
+++ frontend/views/party/_form.php
@@ -5,8 +5,8 @@
         <?= $form->field($model,'phone')->textInput();?>
         <?= $form->field($model,'email')->textInput();?>
         <?= $form->field($model,'street_address')->textInput();?>
-        <?= $form->field($model,'city')->textInput();?>
-        <?= $form->field($model,'location')->textInput();?>
+        <?= $form->field($model,'city')->textInput()->label('City and Village');?>
+        <?= $form->field($model,'location')->textInput()->label('District');?>
         <?= $form->field($model,'state')->textInput();?>
         <?= $form->field($model,'pincode')->hiddenInput(['value'=> 10])->label(false);?>
         <?= $form->field($model,'last_order_id')->hiddenInput(['value'=> 10])->label(false);?>
