a6df679f2a00625377dcbbb2fbcfdc9cb8e8ac85
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-27 13:51:48 +0530

    init

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -13,5 +13,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
 );
-var_dump($config['components']['mailer']); die();
+// var_dump($config['components']['mailer']); die();
 (new yii\web\Application($config))->run();
