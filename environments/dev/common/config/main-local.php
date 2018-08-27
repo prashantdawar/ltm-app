12a1c03fb52e3ba4eb796deffa0cd93b728bd899
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-27 12:51:32 +0530

    init

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -11,13 +11,5 @@ return [
                 $event->sender->createCommand("SET time_zone='Asia/Calcutta")->execute(); 
               },
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
     ],
 ];
