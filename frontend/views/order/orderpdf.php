baa4f082ead29ee5f10537ffb48c3709ceab7a3c
prashantdawar <prashantdawar.2008@gmail.com>
2018-09-05 20:33:36 +0530

    init

--- frontend/views/order/orderpdf.php
+++ frontend/views/order/orderpdf.php
@@ -63,22 +63,9 @@
         <tr>
             <td class="w33"></td>
             <td class="w33 left-line">Tax Invoice</td>
-            <td class="w33 left-line">For Record</td>
+            <td class="w33 left-line">For:</td>
         </tr>
     </table>
-
-    <table>
-        <tr>            
-            <td>Address: <?= 
-                $partyModel->street_address .','. 
-                $partyModel->city.','. 
-                $partyModel->location.','.
-                $partyModel->state.','?>
-                Pincode: <?= $partyModel->pincode ?></td>                       
-        </tr>
-    </table>
-    
-    
     <table>
         <tr>
             <td>Shipped From: 3, Ganesh Market, Near Chopta Bazzar, Inside badsi gate, Hansi, Hisar - 125033, Haryana </td>
@@ -88,33 +75,33 @@
         <tr>
             <td class="w33">State: Haryana</td>
             <td class="w33 left-line">State Code: 06</td>
-            <td class="w33 left-line">GSTIN Number: sfgs897897697</td>
+            <td class="w33 left-line">GSTIN Number: ----</td>
         </tr>
     </table>
     <table>
         <tr>
             <th class="w33">Invoice No. <?= $model->id; ?></th>
-            <th class="w33 left-line"></th>
-            <th class="w33 left-line"></th>
+            <th class="w70 left-line"></th>
+            <!-- <th class="w33 left-line"></th> -->
         </tr>
         <tr>
             <td class="w33">
                 <table>
                     <tr>
-                        <td class="w33">Invoice Date:</td>
+                        <td class="w50">Invoice Date:</td>
                         <td > <?= $model->created_at; ?> </td>
                     </tr>
                     <tr>
-                        <td class="w33">Doc Ref:</td>
+                        <td class="w50">Doc Ref:</td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td class="w33">PO Ref:</td>
+                        <td class="w50">PO Ref:</td>
                         <td></td>
                     </tr>
                     <tr>
-                        <td class="w33">Payment Terms:</td>
-                        <td>COD</td>
+                        <td class="w50">Payment Mode:</td>
+                        <td>CASH</td>
                     </tr>
                 </table>
             </td>
@@ -145,101 +132,50 @@
                     </tr>
                 </table>
             </td> -->
-            <td class="w33 left-line">
-                <table>
+            <td class="w70 left-line">
+            <table>
                     <tr>
-                        <td class="w50">Special Instructions</td>
-                        <td></td>
+                        <th class="w50">Bill to Address: <?= $partyModel->name; ?></th>
                     </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
-    <table>
-        <tr>
-            <td class="w33">Place of Supply: </td>
-            <td class="w33 left-line">State Code: 06</td>
-            <td class="w33 left-line"> State: Haryana</td>
-        </tr>
-    </table>
-    <table>
-        <tr>
-            <th class="w50">Bill to Address: <?= $partyModel->name; ?></th>
-            <th class="w50 left-line">Delivery Address: <?= $partyModel->name; ?></th>
-        </tr>
-        <tr>
-            <td class="w50">
-                <table>
                     <tr>
-                    <td>Address: <?= 
-                        $partyModel->street_address .','. 
-                        $partyModel->city.','. 
-                        $partyModel->location.','.
-                        $partyModel->state.','?>
-                        Pincode: <?= $partyModel->pincode ?>
-                    </td>  
-                    </tr>
-                    <tr><td><br></td></tr>
-                    <tr>
-                        <td>
+                        <td class="w50">
                             <table>
                                 <tr>
-                                    <td class="w33">State (Code):</td>
-                                    <td>Haryana(06)</td>
+                                <td>Address: <?= 
+                                    $partyModel->street_address .','. 
+                                    $partyModel->city.','. 
+                                    $partyModel->location.','.
+                                    $partyModel->state.','?>
+                                    Pincode: <?= $partyModel->pincode ?>
+                                </td>  
                                 </tr>
                                 <tr>
-                                    <td class="w33">PAN No.</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td class="w33">Contact:</td>
-                                    <td> 9996807592</td>
-                                </tr>
-                                <tr>
-                                    <td class="w33">Email:</td>
-                                    <td></td>
+                                    <td>
+                                        <table>
+                                            <tr>
+                                                <td class="w33">State:</td>
+                                                <td> <?= $partyModel->state; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td class="w33">PAN No.:</td>
+                                                <td></td>
+                                            </tr>
+                                            <tr>
+                                                <td class="w33">Contact:</td>
+                                                <td><?= $partyModel->phone; ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td class="w33">Email:</td>
+                                                <td><?= $partyModel->email; ?></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    
                                 </tr>
                             </table>
-                        </td>
-                        
+                        </td>                        
                     </tr>
-                </table>
-            </td>
-            <td class="w50 left-line">
-                <table>
-                    <tr>
-                    <td>Address: <?= 
-                        $partyModel->street_address .','. 
-                        $partyModel->city.','. 
-                        $partyModel->location.','.
-                        $partyModel->state.','?>
-                        Pincode: <?= $partyModel->pincode ?>
-                    </td> 
-                    </tr>
-                    <tr><td><br></td></tr>
-                    <tr>
-                        <td>
-                            <table>
-                                    <tr>
-                                        <td class="w33">State (Code):</td>
-                                        <td>Haryana(06)</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w33">LandMark: </td>
-                                        <td> <?= $partyModel->street_address; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w33">Contact:</td>
-                                        <td> <?= $partyModel->phone; ?></td>
-                                    </tr>
-                                    <tr>
-                                        <td class="w33">Email:</td>
-                                        <td><?= $partyModel->email; ?></td>
-                                    </tr>
-                            </table>
-                        </td>
-                    </tr>
-                </table>
+                </table>                
             </td>
         </tr>
     </table>
@@ -248,7 +184,7 @@
             <th class="w10">Sr. No.</th>
             <th class="w50">Product and Service Description</th>
             <th class="w10">Quantity</th>
-            <th class="w10">IGST</th>
+            <th class="w10"></th>
             <th class="w5"></th>
             <th>Gross Amount</th>
         </tr>
@@ -258,14 +194,14 @@
             <td class="w10"><?= $index+1;  ?></td>
             <td class="w50"><?= $dataItem[$index*2]; ?></td>
             <td class="w10"><?= $dataItem[$index*2+1]; ?></td>
-            <td class="w10">18.0%</td>
+            <td class="w10"></td>
             <td class="w5"></td>
             <td><?= $dataItem[$index*2+1]* $amount; ?></td>
             <?php $subTotal += $dataItem[$index*2+1]* $amount; ?>
         </tr>
          <?php } ?>
         
-        <tr style="height: 50px">
+        <tr style="height: 30px">
             <td class="w10"></td>
             <td class="w50"></td>
             <td class="w10"></td>
@@ -322,69 +258,19 @@
     </table>
     <table>
         <tr>
-            <td>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-            </td>
-        </tr>
-    </table>
-    <table>
-        <tr>
             <td class="w70">
                 Customer Care: 9996807592, Email: sales@mokarte.in
-            </td>
-            <td class="left-line"></td>
-        </tr>
-        <tr>
-            <td class="w70">
-                Good Shipped / sold under this invoice are for personal use and for resale.
-            </td>
-            <td class="left-line"></td>
-        </tr>
-    </table>
-    <table>
-        <tr>
-            <td class="w50">GST payable on Reverse Charge basis.</td>
-            <td class="w20">N.A.</td>
-            <td class="w33 left-line">For Sample Company</td>
-        </tr>
-    </table>
-    <table>
-        <tr>
-            <td class="w70">
+                <br>
+                Good Shipped / sold under this invoice are for personal use and for resale..
+                <br>
                 Declaration: Certified that the particulars given above are true and correct.
-            </td>
-            <td class="left-line"></td>
-        </tr>
-        <tr>
-            <td class="w70">
-                    TDS Declaration: In terms.
-            </td>
-            <td class="left-line"></td>
-        </tr>
-        <tr>
-            <td class="w70">
                 <br>
-                <br>
-                <br>
-                <br>
-                <br>
+                TDS Declaration: N.A.
             </td>
             <th class="left-line">
-                <br>
-                <br>
-                <br>
-                <br>
                 Authorized Signatory
+                <br>
+                <br>                
             </th>
         </tr>
     </table>
