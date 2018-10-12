cf08c4a0c29b1329ceaf50225af0dc42ed3fd4a3
prashantdawar <prashantdawar.2008@gmail.com>
2018-10-12 13:47:42 +0530

    update

--- frontend/views/party/_form.php
+++ frontend/views/party/_form.php
@@ -2,14 +2,14 @@
     <?php $form = \yii\widgets\ActiveForm::begin(); ?>
         <?= $form->field($model,'name')->textInput(['autocomplete' => 'off']);?>
         <?= $form->field($model,'contact_name')->textInput(['autocomplete' => 'off']);?>
-        <?= $form->field($model,'phone')->textInput(['autocomplete' => 'off']);?>
-        <?= $form->field($model,'whatsapp')->textInput(['autocomplete' => 'off']);?>
+        <?= $form->field($model,'phone')->textInput(['type' =>'number','autocomplete' => 'off']);?>
+        <?= $form->field($model,'whatsapp')->textInput(['type' => 'number', 'autocomplete' => 'off']);?>
         <?= $form->field($model,'email')->textInput(['autocomplete' => 'off']);?>
         <?= $form->field($model,'street_address')->textInput();?>
         <?= $form->field($model,'city')->textInput();?>
         <?= $form->field($model,'location')->textInput();?>
         <?= $form->field($model,'state')->textInput();?>
-        <?= $form->field($model,'pincode')->textInput();?>
+        <?= $form->field($model,'pincode')->textInput(['type' => 'number']);?>
         <?= $form->field($model,'gst')->textInput(['autocomplete' => 'off']);?>
 
         <div class="form-group">
