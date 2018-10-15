1baa9889b9cb539aeac7a25d85b9fbe638d1568b
prashantdawar <prashantdawar.2008@gmail.com>
2018-10-15 14:33:42 +0530

    send email changed

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'adminEmail' => 'sales@datapacks.in',
-    'supportEmail' => 'support@datapacks.in',
+    'supportEmail' => 'no-reply@datapacks.in',
     'user.passwordResetTokenExpire' => 3600,
 ];
