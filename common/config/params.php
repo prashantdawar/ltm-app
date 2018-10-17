d074a713b844ced04ea5e1f7756581fefadc5375
prashantdawar <prashantdawar.2008@gmail.com>
2018-10-17 12:46:08 +0530

    send email changed from no-reply to support

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'adminEmail' => 'sales@datapacks.in',
-    'supportEmail' => 'no-reply@datapacks.in',
+    'supportEmail' => 'support@datapacks.in',
     'user.passwordResetTokenExpire' => 3600,
 ];
