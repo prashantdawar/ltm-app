0c5a5210555129228689f6d1916188d4cf1ab9b1
prashantdawar <prashantdawar.2008@gmail.com>
2018-10-18 12:56:59 +0530

    changed header color

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#999999" />
+    <meta name="theme-color" content="#222222" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
