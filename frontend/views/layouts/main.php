834432cdca0d3870a377284a2d440f14a54419fd
prashantdawar <prashantdawar.2008@gmail.com>
2018-10-18 13:14:04 +0530

    address bar fix 3

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,7 +18,6 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="theme-color" content="#222222" />
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
